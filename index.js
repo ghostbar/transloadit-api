ffa6000dcf5a1991ff94e7b91f7a096ab2315359
Jose Luis Rivas <me@ghostbar.co>
2014-02-08 22:43:49 -0430

    index: better vars declares

--- index.js
+++ index.js
@@ -6,12 +6,12 @@
 //
 'use strict';
 
-var Signature = require('./lib/signature'),
-  Assemblies = require('./lib/assemblies'),
-  uri = {
-    base: 'http://api2.transloadit.com',
-    assemblies: '/assemblies'
-  };
+var Signature = require('./lib/signature');
+var Assemblies = require('./lib/assemblies');
+var uri = {
+  base: 'http://api2.transloadit.com',
+  assemblies: '/assemblies'
+};
 
 module.exports = function (opts) {
   return {
