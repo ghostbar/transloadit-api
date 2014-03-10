863d063b7e8071ebcf4b8c16354b228d8bbea68e
Jose Luis Rivas <me@ghostbar.co>
2014-03-10 04:17:31 -0430

    net-test-assembly: finally working

--- test/net-test-assembly.js
+++ test/net-test-assembly.js
@@ -4,28 +4,32 @@
 
 var TransloaditLib = require('../index');
 var config = require('./config.json');
-var should = require('should');
+var should = require('should')
 
 describe('TransloadIt.Assembly Module Network Tests:', function () {
-  describe('', function () {
+  describe('Upload a single file file', function () {
+    var a = new TransloaditLib(config);
+    var path = require('path');
+
+    var files = {
+      'file': {
+        'name': 'Randomness',
+        'path': path.join(__dirname, 'test-image.jpg')
+      }
+    };
+
+    var params = {
+      'template_id': config.template_id 
+    };
+
     it('should post an assembly with a single file', function (done) {
       this.timeout(40000);
 
-      var a = new TransloaditLib(config);
-      var path = require('path');
+      a.assembly.post(files, params, function(err, res) {
+        should.not.exist(err);
 
-      var files = {
-        'file': {
-          'name': 'Randomness',
-          'path': path.join(__dirname, 'dialup-final.png')
-        }
-      };
-
-      var params = {
-        'template_id': '24a7fda83bff4cfc9bc662f585c996b4'
-      };
-
-      a.assembly.post(files, params, done);
+        should.exist(res);
+      });
     });
   });
 });
