823cb35cc4cdd569259fc8b27d954b5c08c1e400
Jose Luis Rivas <me@ghostbar.co>
2014-04-30 04:55:25 -0500

    lib/notifications: initial commit

--- index.js
+++ index.js
@@ -8,15 +8,18 @@
 
 var Signature = require('./lib/signature');
 var Assemblies = require('./lib/assemblies');
+var Notifications = require('./lib/notifications');
 var uri = {
   base: 'http://api2.transloadit.com',
-  assemblies: '/assemblies'
+  assemblies: '/assemblies',
+  notifications: '/assembly_notifications'
 };
 
 module.exports = function (opts) {
   return {
     signature: new Signature(opts),
-    assembly: new Assemblies(opts, uri)
+    assembly: new Assemblies(opts, uri),
+    notification: new Notifications(opts, uri)
   };
 };
 
@@ -26,4 +29,8 @@ module.exports.signature = module.exports.Signature = function (opts) {
 
 module.exports.assembly = module.exports.Assembly = function (opts) {
   return new Assemblies(opts, uri);
+};
+
+module.exports.notification = module.exports.Notification = function (opts) {
+  return new Notifications(opts, uri);
 };
