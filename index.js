19ea59c2da37d2112e5b92974ea660f30ff15f05
Jose Luis Rivas <me@ghostbar.co>
2014-05-04 04:38:15 -0500

    index: loading templates

--- index.js
+++ index.js
@@ -9,17 +9,20 @@
 var Signature = require('./lib/signature');
 var Assemblies = require('./lib/assemblies');
 var Notifications = require('./lib/notifications');
+var Templates = require('./lib/templates');
 var uri = {
   base: 'http://api2.transloadit.com',
   assemblies: '/assemblies',
-  notifications: '/assembly_notifications'
+  notifications: '/assembly_notifications',
+  templates: '/templates'
 };
 
 module.exports = function (opts) {
   return {
     signature: new Signature(opts),
     assembly: new Assemblies(opts, uri),
-    notification: new Notifications(opts, uri)
+    notification: new Notifications(opts, uri),
+    template: new Templates(opts, uri);
   };
 };
 
@@ -33,4 +36,8 @@ module.exports.assembly = module.exports.Assembly = function (opts) {
 
 module.exports.notification = module.exports.Notification = function (opts) {
   return new Notifications(opts, uri);
+};
+
+module.exports.template = module.exports.Template = function (opts) {
+  return new Templates(opts, uri);
 };
